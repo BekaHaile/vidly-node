5ca225619654325151235a04b2b5bdc66fd5f326
Beka Haile <barbeka.tola@gmail.com>
2023-05-17 12:59:26 +0400

    Create, get and delete functions added

--- index.js
+++ index.js
@@ -16,3 +16,37 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
+
+const Course = mongoose.model("Course", courseSchema);
+
+const createCourse = async () => {
+  const course = new Course({
+    name: "Node.js Course",
+    author: "Beka",
+    tags: ["node", "backend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+};
+
+// createCourse();
+
+const getCourses = async () => {
+  const courses = await Course.find({ isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1, author: 1 });
+  console.log(courses);
+};
+
+getCourses();
+
+//delete courses with author Beka
+const deleteCourses = async () => {
+  const result = await Course.deleteMany({ author: "Beka" });
+  console.log(result);
+};
+
+// deleteCourses();
