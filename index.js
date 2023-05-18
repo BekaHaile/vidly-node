3b210fb15727437379e52cb2da4e97346ebd22f1
Beka Haile <barbeka.tola@gmail.com>
2023-05-18 16:09:17 +0400

    Operators added

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ const Course = mongoose.model("Course", courseSchema);
 
 const createCourse = async () => {
   const course = new Course({
-    name: "Node.js Course",
-    author: "Beka",
-    tags: ["node", "backend"],
+    name: "React js Course",
+    author: "Tg",
+    tags: ["react", "frontend"],
     isPublished: true,
   });
 
@@ -31,11 +31,38 @@ const createCourse = async () => {
   console.log(result);
 };
 
-// createCourse();
+createCourse();
 
 const getCourses = async () => {
-  const courses = await Course.find({ isPublished: true })
-    .limit(10)
+  //Comparison Operators in MongoDB are
+  // eq (equal)
+  // ne (not equal)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
+
+  //Logical Operators in MongoDB are
+  // or - or([{author, 'Mosh'}, {isPublished: true}])
+  // and - and([{author, 'Mosh'}, {isPublished: true}])
+
+  //Regular Expressions in MongoDB are
+  // Starts with au - /^au/
+  // Ends with au - /au$/
+  // Contains au - /.*au.*/
+  // Case Insensitive - /au/i
+
+  //Pagination
+  const pageNumber = 2;
+  const pageSize = 10;
+
+  const courses = await Course
+    // find({ isPublished: true }
+    .find({ price: { $gte: 10, $lte: 20 } })
+    .skip((pageNumber - 1) * pageSize) //pagination
+    .limit(pageSize)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1, author: 1 });
   console.log(courses);
