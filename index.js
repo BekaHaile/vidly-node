f61b9f712125c7af6ec93883ce3631a5e0333cf5
Beka Haile <barbeka.tola@gmail.com>
2023-06-13 16:02:39 +0400

    Validation added to tags array

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ const courseSchema = new mongoose.Schema({
     trim: true,
   },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: "A course should have at least one tag.",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
